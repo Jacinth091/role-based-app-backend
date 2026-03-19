f2ed29e0c96643efae93cafd19bb0f29ea619177
Jacinth <jacinthcedricbarral@gmail.com>
2026-03-20 01:57:34 +0800

    feat[Jacinth]: add admin account management routes

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -3,7 +3,7 @@ const {
   authenticateToken,
   authorizeRole,
 } = require("../middlewares/middleware.js");
-const { adminDashboard } = require("../controllers/admin.controller.js");
+const { adminDashboard, getUserById, getAccountsList, createAccount, editAccount, deleteAccount, resetAccountPassword } = require("../controllers/admin.controller.js");
 
 const router = express.Router();
 
@@ -13,5 +13,17 @@ router.get(
   authorizeRole("admin"),
   adminDashboard,
 );
+
+router.get(
+  "/user",
+  authenticateToken,
+  authorizeRole("admin"),
+  getAccountsList,
+);
+router.get('/user/:id', authenticateToken, authorizeRole('admin'), getUserById);
+router.post("/user", authenticateToken, authorizeRole('admin'), createAccount);
+router.patch("/user/:id", authenticateToken, authorizeRole('admin'), editAccount);
+router.delete("/user/:id", authenticateToken, authorizeRole('admin'), deleteAccount);
+router.patch("/user/:id/reset-password", authenticateToken, authorizeRole('admin'), resetAccountPassword);
 
 module.exports = router;
