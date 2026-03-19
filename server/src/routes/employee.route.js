a1235bd15ac0e19ceeb2ee47e96d3bb60c4681d4
Jacinth <jacinthcedricbarral@gmail.com>
2026-03-20 01:57:27 +0800

    feat[Jacinth]: add additional employee CRUD routes

--- server/src/routes/employee.route.js
+++ server/src/routes/employee.route.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { getEmployeeList, getEmployeeById, addNewEmployee, editEmployee, deleteEmployee } = require('../controllers/employee.controller.js');
+const { authenticateToken, authorizeRole } = require('../middlewares/middleware.js');
 
 const router = express.Router();
 
 
-
+router.get('/', authenticateToken, authorizeRole('admin'), getEmployeeList);
+router.post('/', authenticateToken, authorizeRole('admin'), addNewEmployee);
+router.get('/:id', authenticateToken, authorizeRole('admin'), getEmployeeById);
+router.patch('/:id', authenticateToken, authorizeRole('admin'), editEmployee);
+router.delete('/:id', authenticateToken, authorizeRole('admin'), deleteEmployee);
 
 
 
